50bfd2d8d5fa78ba13639dbfbe463ec8e4eae24d
xadler1 <469190@mail.muni.cz>
2026-06-05 11:13:36 +0200

    Move more stuff into functions, add config options to enable only importing media without generating (new) nodes

--- wrapper.py
+++ wrapper.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 import os
+import sys
 import json
 import requests
 import base64
@@ -78,19 +79,16 @@ def issue_request(
             )
             message = f'Workbench {verb} while requesting "{url}".'
             print(message)
-            
 
 
-def process_directory(current_path, parent_id):
+
+def process_directory(current_path, parent_id, auth_token):
     dir_contents = os.listdir(current_path)
 
     for entry in dir_contents:
         current_full_path = os.path.join(current_path, entry)
 
         if (Path(current_full_path).is_dir()):
-            #parent_id,full_path,subtitle
-            #print("NODES: " + str(current_path) + "," + str(current_full_path) + "," +  entry)
-
             # get parent metadata
             metadata = issue_request("GET", "https://archaeo-vault-devel.phil.muni.cz/node/" + str(parent_id) + "?_format=json", auth_token).json()
 
@@ -112,7 +110,7 @@ def process_directory(current_path, parent_id):
                     metadata.pop(field)
 
             metadata["field_member_of"] = [{"target_id": parent_id, "target_type": "node"}]
-            metadata["field_subtitle"] = [{"value": os.path.relpath(current_full_path, root_path)}]
+            metadata["field_subtitle"] = [{"value": os.path.relpath(current_full_path, wrapper_config.get("root_path"))}]
 
             # create intermediary node and get its nid
             post_response = issue_request("POST", "https://archaeo-vault-devel.phil.muni.cz/node?_format=json", auth_token, json_data=metadata).json()
@@ -124,7 +122,7 @@ def process_directory(current_path, parent_id):
 
             new_id = post_response.get("nid")[0]["value"]
 
-            process_directory(Path(current_full_path), new_id)
+            process_directory(Path(current_full_path), new_id, auth_token)
 
         elif (not Path(current_full_path).match('*/.*')):
             extension = entry.split(".")[-1]
@@ -144,24 +142,61 @@ def process_directory(current_path, parent_id):
                 print("unknown extension in file: " + current_full_path)
 
 
+def check_config(wrapper_config):
+    required = ["root_id", "root_path", "auth_token"]
+    for entry in required:
+        if wrapper_config.get(entry) is None:
+            print("Required config entry '" + entry + "' is either empty or missing.")
+            sys.exit(1)
+
+
+def load_config(wrapper_config_name):
+    with open(wrapper_config_name, mode="r") as wrapper_config_file:
+        try:
+            wrapper_config = yaml.load(wrapper_config_file)
+            check_config(wrapper_config)
+            return wrapper_config
+        except YAMLError as exc:
+            print("There appears to be a YAML syntax error in your configuration file")
+            sys.exit(1)
+
+def write_config_files():
+    with open("__dm-wrapper_used_files.json", mode="w") as file:
+        json.dump(list(used_files), file)
+
+    with open("__dm-wrapper_csv_config_dict.json", mode="w") as file:
+        json.dump(csv_config_dict, file)
+
+
+def load_used_files():
+    if not os.path.exists("__dm-wrapper_used_files.json"):
+        print("No generated config files for workbench exist, please rerun this script with 'generate_import_files: true' set in 'wrapper_config.yml'")
+        sys.exit(1)
+
+
+    with open("__dm-wrapper_used_files.json", mode="r") as file:
+        json_data = json.load(file)
+        return set(json_data)
+
+def load_csv_config_dict():
+    if not os.path.exists("__dm-wrapper_used_files.json"):
+        print("No generated config files for workbench exist, please rerun this script with 'generate_import_files: true' set in 'wrapper_config.yml'")
+        sys.exit(1)
+
+    with open("__dm-wrapper_csv_config_dict.json", mode="r") as file:
+        json_data = json.load(file)
+        return json_data
 
 
 yaml = YAML()
-root_path = ""
-root_id = 0
-auth_token = ""
+wrapper_config = load_config("wrapper_config.yml")
 
-
-with open("wrapper_config.yml", mode="r") as wrapper_config_file:
-    try:
-        wrapper_config = yaml.load(wrapper_config_file)
-        root_id = wrapper_config.get("root_id")
-        root_path = Path(wrapper_config.get("root_path"))
-        auth_token = wrapper_config.get("auth_token")
-    except YAMLError as exc:
-        print("There appears to be a YAML syntax error in your configuration file")
-        exit() 
-
+jwt_parsed = json.loads(base64.standard_b64decode(wrapper_config.get("auth_token").split(".")[1]))
+id_field = "file"
+use_workbench_permissions = True
+username = jwt_parsed.get("sub")
+host = jwt_parsed.get("iss")
+task = "add_media"
 
 
 models = dict()
@@ -183,46 +218,48 @@ models.update({"image": {"png", "gif", "jpg", "jpeg"}})
 models.update({"file": {"txt", "rtf", "doc", "docx", "ppt", "pptx", "xls", "xlsx", "pdf", "odf", "odg", "odp", "ods", "odt", "fodt", "fods", "fodp", "fodg", "key", "numbers", "pages", "tiff", "tif", "jp2", "xml", "zip", "vtt", "csv", "mdb", "accdb"}})
 #models.update({"file": {"txt", "rtf", "doc", "docx", "ppt", "pptx", "xls", "xlsx", "pdf", "odf", "odg", "odp", "ods", "odt", "fodt", "fods", "fodp", "fodg", "key", "numbers", "pages", "tiff", "tif", "jp2", "xml", "zip", "vtt", "csv", "mdb", "accdb", "raw"}})
 
-jwt_parsed = json.loads(base64.standard_b64decode(auth_token.split(".")[1]))
 
-id_field = "file"
-use_workbench_permissions = True
-username = jwt_parsed.get("sub")
-host = jwt_parsed.get("iss")
-task = "add_media"
 
 config_dict.update({"task": task})
 config_dict.update({"host": host})
 config_dict.update({"username": username})
 # workbench checks for this, ripping it out would be needlessly difficult
 config_dict.update({"password": "obviouslynotapassword"})
-config_dict.update({"auth_token": auth_token})
+config_dict.update({"auth_token": wrapper_config.get("auth_token")})
 config_dict.update({"use_workbench_permissions": use_workbench_permissions})
 config_dict.update({"id_field": id_field})
 config_dict.update({"media_type_file_fields": {"csv": "field_media_file", "3d_model": "field_media_file", "geospatial_data": "field_media_file"}})
 
-for key in models:
-    csv_filename = "__dm-wrapper_media_csv_" + key + ".csv"
-    config_filename = "__dm-wrapper_media_config_" + key + ".yml"
+if wrapper_config.get("generate_import_files"):
+    for key in models:
+        csv_filename = "__dm-wrapper_media_csv_" + key + ".csv"
+        config_filename = "__dm-wrapper_media_config_" + key + ".yml"
 
-    with open(csv_filename, mode="w") as media_csv:
-        media_csv.write("node_id,file\n")
+        with open(csv_filename, mode="w") as media_csv:
+            media_csv.write("node_id,file\n")
 
-    with open(config_filename, mode="w") as media_config:
-        config_dict.update({"media_type": key})
-        config_dict.update({"input_csv": str(Path(csv_filename).absolute())})
-        yaml.dump(config_dict, media_config)
-        csv_config_dict.update({csv_filename: config_filename})
+        with open(config_filename, mode="w") as media_config:
+            config_dict.update({"media_type": key})
+            config_dict.update({"input_csv": str(Path(csv_filename).absolute())})
+            yaml.dump(config_dict, media_config)
+            csv_config_dict.update({csv_filename: config_filename})
 
+    # absolute path to dataset root directory; drupal node id of main dataset entry created beforehand
+    process_directory(wrapper_config.get("root_path"), wrapper_config.get("root_id"), wrapper_config.get("auth_token"))
 
+    write_config_files()
+else:
+    # load used files from file
+    used_files = load_used_files()
+    csv_config_dict = load_csv_config_dict()
 
+if len(used_files) == 0 or len(csv_config_dict) == 0:
+    print("No generated config files for workbench exist, please rerun this script with 'generate_import_files: true' set in 'wrapper_config.yml'")
 
-# absolute path to dataset root directory; drupal node id of main dataset entry created beforehand
-process_directory(root_path, root_id)
 
 for csv in used_files:
     ret = subprocess.run(["./workbench", "--config", csv_config_dict.get(csv), "--check"])
-    if ret.returncode == 0:
+    if ret.returncode == 0 and wrapper_config.get("import_media"):
         subprocess.run(["./workbench", "--config", csv_config_dict.get(csv)])
 
 
